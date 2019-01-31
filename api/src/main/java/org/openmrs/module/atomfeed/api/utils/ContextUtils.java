07e035c087d7c59bcaaf369c3ce7b8a1ac77c97e
areklalo <alalo@soldevelo.com>
2019-01-31 14:31:02 +0100

    SYNCT-100: Added functionality used to add custom feed builder

--- api/src/main/java/org/openmrs/module/atomfeed/api/utils/ContextUtils.java
+++ api/src/main/java/org/openmrs/module/atomfeed/api/utils/ContextUtils.java
@@ -11,6 +11,9 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
+import org.openmrs.OpenmrsObject;
+import org.openmrs.module.atomfeed.api.converter.FeedBuilder;
+
 public class ContextUtils {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ContextUtils.class);
@@ -47,6 +50,21 @@ public class ContextUtils {
 			LOGGER.debug("Could not fetch '{}' component", ex);
 			return null;
 		}
+	}
+
+	public static FeedBuilder getFeedBuilder(OpenmrsObject openmrsObject) {
+		FeedBuilder builder = getCustomFeedBuilder(openmrsObject);
+		if (builder == null) {
+			builder = getRegisteredComponentSafely(AtomfeedConstants.DEFAULT_FEED_BUILDER, FeedBuilder.class);
+		}
+		return builder;
+	}
+
+	private static FeedBuilder getCustomFeedBuilder(OpenmrsObject openmrsObject) {
+		String feedBuilderBeanId = AtomfeedConstants.FEED_BUILDER_BEAN_ID_PREFIX +
+				openmrsObject.getClass().getSimpleName() + AtomfeedConstants.FEED_BUILDER_BEAN_ID_SUFIX;
+
+		return getRegisteredComponentSafely(feedBuilderBeanId, FeedBuilder.class);
 	}
 
 	private ContextUtils() { }
